8d64ff3a0ccd93fd3b9b9413f7f376510df053d4
Sara Elsa <sara-etx@outlook.com>
2022-12-29 02:31:30 -0500

    Allow creating message bus reply receiver from message bus client

--- src/SETech.Messaging.MessageBus/Client/IMessageBusClient.cs
+++ src/SETech.Messaging.MessageBus/Client/IMessageBusClient.cs
@@ -1,3 +1,4 @@
+using SETech.Messaging.MessageBus.Primitives;
 using SETech.Messaging.MessageBus.Receiver;
 using SETech.Messaging.MessageBus.Sender;
 
@@ -27,4 +28,11 @@ public interface IMessageBusClient
     /// <param name="subscriptionName">The name of the subscription to receive messages from.</param>
     /// <returns>The created <see cref="IMessageBusReceiver{TPayload}"/>.</returns>
     public IMessageBusReceiver<TPayload> CreateReceiver<TPayload>(string topicName, string subscriptionName);
+
+    /// <summary>Creates a <see cref="IMessageBusReplyReceiver{TPayload}"/> for a queue.</summary>
+    /// <typeparam name="TPayload">The payload type to receive.</typeparam>
+    /// <param name="queueName">The name of the queue to receive messages from.</param>
+    /// <returns>The created <see cref="IMessageBusReplyReceiver{TPayload}"/>.</returns>
+    public IMessageBusReplyReceiver<TPayload> CreateReplyReceiver<TPayload>(string queueName)
+        where TPayload : ICorrelatedMessagePayload;
 }
