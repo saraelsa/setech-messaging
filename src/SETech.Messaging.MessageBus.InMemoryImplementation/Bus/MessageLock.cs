6c342eab06efbf801072ef99fb9a97c93a32cbe5
Sara Elsa <sara-etx@outlook.com>
2022-12-30 15:13:52 -0500

    Update message lock implementation and add comments

--- src/SETech.Messaging.MessageBus.InMemoryImplementation/Bus/MessageLock.cs
+++ src/SETech.Messaging.MessageBus.InMemoryImplementation/Bus/MessageLock.cs
@@ -2,18 +2,25 @@ using SETech.Messaging.MessageBus.Receiver;
 
 namespace SETech.Messaging.MessageBus.InMemoryImplementation.Bus;
 
+/// <summary>Handles locking a message for a period of time.</summary>
 public sealed class MessageLock
 {
+    /// <summary>The duration for which the message is locked.</summary>
     public TimeSpan LockDuration { get; private init; }
 
-    public bool Expired => _expired;
+    /// <summary>Whether the lock has expired.</summary>
+    public bool Expired { get; private set; }
 
+    /// <summary>Whether the lock has been cleared.</summary>
+    public bool Cleared { get; private set; }
+
+    /// <summary>Raised when the lock has expired.</summary>
     public event EventHandler? OnExpire;
-    
-    private DateTimeOffset _expiresOn;
-    private bool _expired = false;
-    private bool _cleared = false;
 
+    /// <summary>The time at which the lock expires.</summary>
+    private DateTimeOffset _expiresOn;
+
+    /// <summary>Creates a <see cref="MessageLock"/>.</summary>
     public MessageLock(TimeSpan lockDuration)
     {
         LockDuration = lockDuration;
@@ -21,25 +28,31 @@ public sealed class MessageLock
         CheckForExpiry();
     }
 
+    /// <summary>
+    ///     Wraps an action in a handler that will throw <see cref="LockExpiredException"/> if the message is expired,
+    ///     <see cref="InvalidOperationException"/> if the message is cleared, and clears the message otherwise.
+    /// </summary>
     public Action CreateHandler(Action protectedAction) =>
         () =>
         {
-            if (_expired)
+            if (Expired)
                 throw new LockExpiredException();
 
-            if (_cleared)
-                throw new InvalidOperationException();
-            
-            _cleared = true;
+            if (Cleared)
+                throw new InvalidOperationException("The message has already been settled.");
+
+            Cleared = true;
 
             protectedAction();
         };
     
+    /// <summary>Renews the lock for <see cref="LockDuration"/>.</summary>
     public void Renew()
     {
         _expiresOn = DateTimeOffset.Now + LockDuration;
     }
 
+    /// <summary>Checks whether the lock has expired and handles the expiry if so.</summary>
     private void CheckForExpiry()
     {
         if (DateTimeOffset.Now < _expiresOn)
@@ -48,9 +61,9 @@ public sealed class MessageLock
             return;
         }
 
-        if (!_cleared)
+        if (!Cleared)
         {
-            _expired = true;
+            Cleared = true;
 
             if (OnExpire is not null)
                 OnExpire(this, EventArgs.Empty);
