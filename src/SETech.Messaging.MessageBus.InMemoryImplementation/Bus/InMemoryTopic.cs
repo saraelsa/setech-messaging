fe2cfd600ce79c4645f527a607635b7dc7b17ccb
Sara Elsa <sara-etx@outlook.com>
2022-12-30 09:18:21 -0500

    Allow creating and destroying subscriptions to in-memory topics

--- src/SETech.Messaging.MessageBus.InMemoryImplementation/Bus/InMemoryTopic.cs
+++ src/SETech.Messaging.MessageBus.InMemoryImplementation/Bus/InMemoryTopic.cs
@@ -4,12 +4,27 @@ namespace SETech.Messaging.MessageBus.InMemoryImplementation.Bus;
 
 public class InMemoryTopic<TPayload> : InMemoryQueue<TPayload>
 {
-    protected IDictionary<string, InMemoryQueue<TPayload>> Subscriptions =
+    protected IDictionary<string, InMemoryQueue<TPayload>> _subscriptions =
         new Dictionary<string, InMemoryQueue<TPayload>>();
+    
+    public IReadOnlyDictionary<string, InMemoryQueue<TPayload>> Subscriptions => _subscriptions.AsReadOnly();
     
     public InMemoryTopic()
     {
         Receive(ReceiverFunction);
+    }
+
+    public InMemoryQueue<TPayload> CreateSubscription(string subscriptionName)
+    {
+        if (!_subscriptions.ContainsKey(subscriptionName))
+            _subscriptions[subscriptionName] = new InMemoryQueue<TPayload>();
+        
+        return _subscriptions[subscriptionName];
+    }
+
+    public void DestroySubscription(string subscriptionName)
+    {
+        _subscriptions.Remove(subscriptionName);
     }
 
     protected void ReceiverFunction(ReceivedBusMessage<TPayload> receivedMessage, ReceivedMessageActions actions)
