969a8a5d7d05314afda4b64f80cc90c1c34aec12
Sara Elsa <sara-etx@outlook.com>
2023-01-01 07:02:17 -0500

    Write tests for peek methods

--- tests/SETech.Messaging.MessageBus.InMemoryImplementation.UnitTests/InMemoryQueue_UnitTests.cs
+++ tests/SETech.Messaging.MessageBus.InMemoryImplementation.UnitTests/InMemoryQueue_UnitTests.cs
@@ -77,10 +77,88 @@ public sealed class InMemoryQueue_UnitTests
             actions.Complete();
         });
 
-        Assert.NotNull(message1);
-        Assert.Equal(payload1, message1.Payload);
+        Assert.NotNull(receivedMessage1);
+        Assert.Equal(payload1, receivedMessage1.Payload);
   
-        Assert.NotNull(message2);
-        Assert.Equal(payload2, message2.Payload);
+        Assert.NotNull(receivedMessage2);
+        Assert.Equal(payload2, receivedMessage2.Payload);
+    }
+
+    [Fact]
+    public void Peek_NoAvailableMessages_ReturnsNull()
+    {
+        var receivedMessage = testQueue.Peek(0);
+
+        Assert.Null(receivedMessage);
+    }
+
+    [Fact]
+    public void Peek_SingleMessage_Works()
+    {
+        var (message, payload) = CreateTestMessage();
+
+        testQueue.Publish(message);
+
+        var receivedMessage = testQueue.Peek(fromSequenceNumber: 0);
+
+        Assert.NotNull(receivedMessage);
+        Assert.Equal(payload, receivedMessage.Payload);
+    }
+
+    [Fact]
+    public void Peek_MultipleMessages_SkipByMinimumSequenceNumber_Works()
+    {
+        var (message1, payload1) = CreateTestMessage();
+        var (message2, payload2) = CreateTestMessage();
+
+        testQueue.Publish(message1);
+        testQueue.Publish(message2);
+
+        var receivedMessage = testQueue.Peek(fromSequenceNumber: 1);
+
+        Assert.NotNull(receivedMessage);
+        Assert.Equal(payload2, receivedMessage.Payload);
+    }
+
+    [Fact]
+    public void PeekMany_MultipleMessages_NoAvailableMessages_ReturnsEmptyCollection()
+    {
+        var receivedMessages = testQueue.PeekMany(fromSequenceNumber: 0, maximumResults: 1);
+
+        Assert.Empty(receivedMessages);
+    }
+
+    [Fact]
+    public void PeekMany_MultipleMessages_AllMessages_Works()
+    {
+        var (message1, payload1) = CreateTestMessage();
+        var (message2, payload2) = CreateTestMessage();
+
+        testQueue.Publish(message1);
+        testQueue.Publish(message2);
+
+        var receivedMessages = testQueue.PeekMany(fromSequenceNumber: 0, maximumResults: 2);
+
+        Assert.Equal(2, receivedMessages.Count);
+        Assert.Equal(payload1, receivedMessages[0].Payload);
+        Assert.Equal(payload2, receivedMessages[1].Payload);
+    }
+
+    [Fact]
+    public void PeekMany_MultipleMessages_SkipByMinimumSequenceNumber_Works()
+    {
+        var (message1, payload1) = CreateTestMessage();
+        var (message2, payload2) = CreateTestMessage();
+        var (message3, payload3) = CreateTestMessage();
+
+        testQueue.Publish(message1);
+        testQueue.Publish(message2);
+        testQueue.Publish(message3);
+
+        var receivedMessages = testQueue.PeekMany(fromSequenceNumber: 1, maximumResults: 2);
+
+        Assert.Equal(2, receivedMessages.Count);
+        Assert.Equal(payload2, receivedMessages[0].Payload);
+        Assert.Equal(payload3, receivedMessages[1].Payload);
     }
 }
